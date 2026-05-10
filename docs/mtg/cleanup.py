9980964965ad8649ada2727c3bf2a4f47fe3cfae
Nicholas Moser <NicholasMoser56@gmail.com>
2026-05-10 09:34:09 -0500

    Remove unused images

---
+++ docs/mtg/cleanup.py
@@ -0,0 +1,25 @@
+import os
+
+all_images = []
+for root, dirs, files in os.walk('.'):
+    for file in files:
+        if file not in ['cleanup.py', 'deck.md']:
+            all_images.append(file)
+
+used_images = []
+with open('deck.md', 'r', encoding='utf-8') as deck:
+    for line in deck.readlines():
+        if line.startswith('![alt text]'):
+            curr = line.strip()
+            curr = curr.replace('![alt text](', '')
+            curr = curr.replace('<', '')
+            curr = curr.replace('>', '')
+            curr = curr.replace(')', '')
+            used_images.append(curr)
+
+for image in all_images:
+    if image in used_images:
+        print(f'Used {image}')
+    else:
+        print(f'Unused {image}')
+        os.remove(image)
